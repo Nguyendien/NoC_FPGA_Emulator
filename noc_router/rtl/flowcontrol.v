6c2e663514ea82ae02057e476fe22f11bb4795a2
Behrad Niazmand <bniazmand@users.noreply.github.com>
2016-03-17 17:59:12 +0200

    Update flowcontrol.v
    Internal Ready signals are removed from the flow control module in order to make the logic simpler and remove redundancy.

--- noc_router/rtl/flowcontrol.v
+++ noc_router/rtl/flowcontrol.v
@@ -21,17 +21,8 @@ module flowcontrol(rst,
   
   output reg  Lready_out, Nready_out, Eready_out, Wready_out, Sready_out;                // Ready signal send to Arbiter, Previous router
   
-  // Declaring the local variables
-  wire        Lready, Nready, Eready, Wready, Sready;
-  
-  assign Lready = Lready_in && Lport;
-  assign Nready = Nready_in && Nport;
-  assign Eready = Eready_in && Eport;
-  assign Wready = Wready_in && Wport;
-  assign Sready = Sready_in && Sport;
-  
   // Ready_out is asserted whenever the selected output port and the corresponding output FIFO is also ready enough
-  always @(rst, Lready, Nready, Eready, Wready, Sready, Lready_out, Nready_out, Eready_out, Wready_out, Sready_out) begin
+  always @(rst, Lready, Nready, Eready, Wready, Sready, Lready_in, Nready_in, Eready_in, Wready_in, Sready_in, Lport, Nport, Eport, Wport, Sport) begin
     if (rst) begin
       Lready_out <= 1'b0;
       Nready_out <= 1'b0;
@@ -40,50 +31,11 @@ module flowcontrol(rst,
       Sready_out <= 1'b0;
     end
     else begin
-      case(1'b1)
-        Lready : begin
-          Lready_out <= 1'b1;
-          Nready_out <= 1'b0;
-          Eready_out <= 1'b0;
-          Wready_out <= 1'b0;
-          Sready_out <= 1'b0;
-        end
-        Nready : begin
-          Lready_out <= 1'b0;
-          Nready_out <= 1'b1;
-          Eready_out <= 1'b0;
-          Wready_out <= 1'b0;
-          Sready_out <= 1'b0;
-        end
-        Eready : begin
-          Lready_out <= 1'b0;
-          Nready_out <= 1'b0;
-          Eready_out <= 1'b1;
-          Wready_out <= 1'b0;
-          Sready_out <= 1'b0;
-        end
-        Wready : begin
-          Lready_out <= 1'b0;
-          Nready_out <= 1'b0;
-          Eready_out <= 1'b0;
-          Wready_out <= 1'b1;
-          Sready_out <= 1'b0;
-        end
-        Sready : begin
-          Lready_out <= 1'b0;
-          Nready_out <= 1'b0;
-          Eready_out <= 1'b0;
-          Wready_out <= 1'b0;
-          Sready_out <= 1'b1;
-        end
-        default: begin
-          Lready_out <= 1'b0;
-          Nready_out <= 1'b0;
-          Eready_out <= 1'b0;
-          Wready_out <= 1'b0;
-          Sready_out <= 1'b0;
-        end
-      endcase
+      Lready_out <= Lready_in && Lport;
+      Nready_out <= Nready_in && Nport;
+      Eready_out <= Eready_in && Eport;
+      Wready_out <= Wready_in && Wport;
+      Sready_out <= Sready_in && Sport;
     end
   end
   
