ed46097bcf390c3f4dc7989971cefaf68f4d0e37
vineethgovind <vineethgovind@gmail.com>
2016-04-14 14:30:19 +0300

    Update RNI.vhd

--- noc_network_interface/RNI.vhd
+++ noc_network_interface/RNI.vhd
@@ -93,11 +93,11 @@ Port
 	
 	end component;
 Signal router_data_ram,fifo_data_out,PE_data_ram,fifo_data_out1  :std_logic_vector(31 downto 0);
-Signal PE_ready_sig,NI2ROUTER_READY_sig,NI2PE_READY_sig:std_logic;
+
 Signal  Router_length_in_sig,Router_length_out_sig,PE_length_in_sig,PE_length_out_sig:std_logic_vector(11 downto 0);
 signal wr_en,rd_en,empty,almost_full,write_data_ctrl,read_data_ctrl,wr_en1,rd_en1,empty1,almost_full1,write_data_ctrl1,read_data_ctrl1:std_logic;
 Signal Router_data_no_length,PE_data_no_length:std_logic_vector(19 downto 0);
-Signal Router_write_sig,PE_write_sig,ROUTER_ready_sig:std_logic;
+
 
 FOR WRITE_CONTROLLER_R2PE : Writefsm USE  ENTITY work.Writefsm;
 FOR FIFO_R2PE : FIFO USE  ENTITY work.FIFO;
@@ -110,47 +110,27 @@ FOR READ_CONTROLLER_PE2R : ReadFsm USE  ENTITY work.Readfsm;
 For Out_reg_PE2R:Out_reg USE ENTITY work.Out_reg;
 Begin
 
----full_Sig<=full;
+
 Router_length_in_sig<=R2NI_DATA(28 DOWNTO 17);
-Router_write_sig<=ROUTER_WRITE;
+
 Router_data_no_length<=R2NI_DATA(31 DOWNTO 29)&R2NI_DATA(16 DOWNTO 0);
 
-NI2ROUTER_READY<=NI2ROUTER_READY_sig;
 
-PE_ready_sig<=PE_READY;
-
---process (clk)
---begin
---if clk'event and clk = '1' then
-			--if rst = '0' then
-			--router_data_ram<="00000000000000000000000000000000";
-			--else
 router_data_ram<=R2NI_DATA(31 DOWNTO 29)&router_length_out_sig&R2NI_DATA(16 DOWNTO 0);
---end if;
---end if;
---end process;
+
 
 PE_length_in_sig<=PE2NI_DATA(28 DOWNTO 17);
-PE_write_sig<=PE_WRITE;
+
 PE_data_no_length<=PE2NI_DATA(31 DOWNTO 29)&PE2NI_DATA(16 DOWNTO 0);
 
-NI2PE_READY<=NI2PE_READY_sig;
 
-ROUTER_ready_sig<=ROUTER_READY;
 
---process (clk)
---begin
---if clk'event and clk = '1' then
-			--if rst = '0' then
-			--pe_data_ram<="00000000000000000000000000000000";
-			--else
+
 PE_data_ram<=PE2NI_DATA(31 DOWNTO 29)&PE_length_out_sig&PE2NI_DATA(16 DOWNTO 0);
---end if;
---end if;
---end process;
+
 
 WRITE_CONTROLLER_R2PE :Writefsm
-Port Map(Router_length_in_sig,clk,rst,almost_full,Router_write_sig,wr_en,NI2ROUTER_READY_sig,write_data_ctrl, Router_length_out_sig);
+Port Map(Router_length_in_sig,clk,rst,almost_full,Router_write,wr_en,NI2ROUTER_READY,write_data_ctrl, Router_length_out_sig);
 
 FIFO_R2PE :FIFO
 Port Map(clk,rst,wr_en,rd_en,router_data_ram,fifo_data_out,empty,almost_full);
@@ -159,11 +139,11 @@ Out_reg_R2PE:Out_reg
 Port Map(clk,rst,fifo_data_out,write_data_ctrl,read_data_ctrl,NI2PE_DATA);
 
 READ_CONTROLLER_R2PE:Readfsm
-Port Map(Clk,rst,empty,PE_ready_sig,rd_en,read_data_ctrl,NI2PE_WRITE);
+Port Map(Clk,rst,empty,PE_ready,rd_en,read_data_ctrl,NI2PE_WRITE);
 
 
 WRITE_CONTROLLER_PE2R :Writefsm
-Port Map(PE_length_in_sig,clk,rst,almost_full1,PE_write_sig,wr_en1,NI2PE_READY_sig,write_data_ctrl1, PE_length_out_sig);
+Port Map(PE_length_in_sig,clk,rst,almost_full1,PE_write,wr_en1,NI2PE_READY,write_data_ctrl1, PE_length_out_sig);
 
 FIFO_PE2R :FIFO
 Port Map(clk,rst,wr_en1,rd_en1,PE_data_ram,fifo_data_out1,empty1,almost_full1);
@@ -172,5 +152,5 @@ Out_reg_PE2R:Out_reg
 Port Map(clk,rst,fifo_data_out1,write_data_ctrl1,read_data_ctrl1,NI2ROUTER_DATA);
 
 READ_CONTROLLER_PE2R:Readfsm
-Port Map(Clk,rst,empty1,ROUTER_ready_sig,rd_en1,read_data_ctrl1,NI2ROUTER_WRITE);
+Port Map(Clk,rst,empty1,ROUTER_ready,rd_en1,read_data_ctrl1,NI2ROUTER_WRITE);
 END FUN;
